e8ba15e848d57e272951c41d1c70a04829f07810
Nithin Daniel <nithindaniel2018@gmail.com>
2024-02-29 11:20:13 +0530

    done user registration

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { User } = require('../models/users')
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 router.get('/login',(req,res)=>{
     res.json({ message: 'Login' });
@@ -35,8 +36,21 @@ router.post('/register',async(req,res)=>{
     //         return res.status(400).json({ message: err.message })
     //     }
     // }
-    let users = await User.find({email:body_email}).exec();
-    console.log(users);
+    // let users = await User.find({email:body_email}).exec();
+    // console.log(users);
+    try {
+        const salt = await bcrypt.genSalt(10)
+        const password = await bcrypt.hash(req.body.password, salt)
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: password
+        })
+        await user.save()
+        return res.status(201).json(user)
+    } catch (err) {
+        return res.status(400).json({ message: err.message })
+    }
     
 });
 module.exports = router;
