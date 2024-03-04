da60d3c1815c1c49d7b298a2593ae9b360c1ddbe
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2024-03-04 18:28:22 +0530

    added validation in where the user is already exists or not

--- routes/users.js
+++ routes/users.js
@@ -35,48 +35,26 @@ router.post('/login',async(req,res)=>{
 });
 
 router.post('/register',async(req,res)=>{
-    // console.log(req.body);
-    // res.json({ message: req.body });
-        // let user = await UserSchema.findOne({  where: { email: email }  })
     body_email = req.body.email
-    // let user = await User.find({email}).exec((err,doc)=>{
-    //     if (err){
-    //         console.log(err);
-    //     }
-    // });
-    // console.log(user);
-    // if (user) {
-    //     return res.status(400).send('User already exisits. Please sign in')
-    // } else {
-    //     try {
-    //         const salt = await bcrypt.genSalt(10)
-    //         const password = await bcrypt.hash(req.body.password, salt)
-    //         const user = new User({
-    //             name: req.body.name,
-    //             email: req.body.email,
-    //             password: password
-    //         })
-    //         await user.save()
-    //         return res.status(201).json(user)
-    //     } catch (err) {
-    //         return res.status(400).json({ message: err.message })
-    //     }
-    // }
-    // let users = await User.find({email:body_email}).exec();
-    // console.log(users);
-    try {
-        const salt = await bcrypt.genSalt(10)
-        const password = await bcrypt.hash(req.body.password, salt)
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: password
-        })
-        await user.save()
-        return res.status(201).json({ message:"User created" })
-    } catch (err) {
-        return res.status(400).json({ message: err.message })
+    let user = await User.findOne({ email: body_email })
+    if (user) {
+        return res.status(400).send('User already exisits. Please sign in')
+    } else {
+        try {
+            const salt = await bcrypt.genSalt(10)
+            const password = await bcrypt.hash(req.body.password, salt)
+            const user = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: password
+            })
+            await user.save()
+            return res.status(201).json({message:"User Created Successfully"})
+        } catch (err) {
+            return res.status(400).json({ message: err.message })
+        }
     }
+}
     
-});
+);
 module.exports = router;
