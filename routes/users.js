b92cf3e9521dc49f5f442ea8fd35d8d613af2910
Nithin Daniel <nithindaniel2018@gmail.com>
2024-04-18 18:22:17 +0530

    login user userdetails removed some datas

--- routes/users.js
+++ routes/users.js
@@ -27,8 +27,7 @@ router.post('/login', async (req, res) => {
                     return res.status(400).json({ message: "Incorrect email or password." })
                 }
                 const token = jwt.sign({ email: email }, process.env.SECRET_KEY, { expiresIn: '20d' })
-                const userdata = await UserDetails.findOne({ email: user.email })
-
+                const userdata = await UserDetails.findOne({ email: user.email }).select('-_id -__v')
                 return res.status(200).json({ UserData: userdata, Token: token })
             } else {
                 return res.status(400).json({ message: "User not verified" })
