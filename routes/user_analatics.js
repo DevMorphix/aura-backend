448101327aa7da51e31803e0a31a3abf1c391ece
Nithin Daniel <nithindaniel2018@gmail.com>
2024-05-19 02:01:58 +0530

    period end

--- routes/user_analatics.js
+++ routes/user_analatics.js
@@ -16,10 +16,25 @@ router.post('/period-start',isAuthenticated,isUserValidate,async (req,res) =>{
         const { start_date } = req.body;
         const newPeriods = new PeriodsDates({
             user:current_user,
-            periods_start:start_date
+            periods_start:start_date,
+            periods_end:start_date
         })
         await newPeriods.save()
-        return res.status(200).json({ message: "Periods Start date added"})
+        return res.status(200).json({ message: "Periods Start date updated"})
+    }catch(error){
+        console.log(error);
+        return res.status(400).json({ message: err.message });
+    }
+});
+
+router.post('/period-end',isAuthenticated,isUserValidate,async (req,res) =>{
+    try{
+        const current_user = req.user["email"]
+        const { end_date } = req.body;
+        const periods = await PeriodsDates.findOne({user:current_user})
+        periods.periods_end = end_date
+        await periods.save()
+        return res.status(200).json({ message: "Periods End date updated"})
     }catch(error){
         console.log(error);
         return res.status(400).json({ message: err.message });
