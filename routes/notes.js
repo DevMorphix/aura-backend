2f4ee44dc335f60399a1bee83c23fe9a71d7e4dc
Nithin Daniel <nithindaniel2018@gmail.com>
2024-05-18 13:52:09 +0530

    get all notes

--- routes/notes.js
+++ routes/notes.js
@@ -6,6 +6,19 @@ const mongoose = require('mongoose');
 const isAuthenticated = require('../middlware/auth');
 const { v4: uuidv4 } = require('uuid');
 
+
+router.get('/', isAuthenticated, async (req, res) => {
+    try {
+        const current_user = req.user["email"];
+        const note = await Notes.find({user:current_user}).select('-_id -__v')
+        return res.status(200).json({ Note: note})
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({ message: err.message })
+
+    }
+});
+
 router.post('/add', isAuthenticated, async (req, res) => {
     try {
         const user = req.user["email"];
@@ -29,7 +42,6 @@ router.post('/add', isAuthenticated, async (req, res) => {
 router.get('/:id', isAuthenticated, async (req, res) => {
     try {
         const note = await Notes.findOne({note_id:req.params.id}).select('-_id -__v')
-        console.log(note);
         return res.status(200).json({ Note: note})
     } catch (err) {
         console.log(err);
