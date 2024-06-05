3aa2a1e234cd8e7b6debcf829d841dc76495cb97
Nithin Daniel <nithindaniel2018@gmail.com>
2024-06-05 12:37:59 +0530

    get appoinment users data issue

--- routes/appoinment.js
+++ routes/appoinment.js
@@ -92,12 +92,20 @@ router.get('/get-appoinments-users', isAuthenticated, isUserValidate, async (req
     try {
         const current_user = req.user["email"];
         const appoinment = await Appoinments.find({ user: current_user, appointment_status: { $ne: 'rejected' } }).select('-_id -__v -created_at -updated_at')
-        const doctor_phone_number = await UserDetails.find({ email: appoinment[0].doctor_email })
-        
-        if (appoinment === null) {
+        // console.log(appoinment);
+        // console.log(doctor_phone_number);
+        // if (doctor_phone_number) {
+        //     doctor_phone_number = doctor_phone_number
+        // } else {
+        //     doctor_phone_number = null
+        // }
+        // return res.status(200).json({ message: "No Appoinment Booked" });
+
+        if (appoinment.length === 0) {
             return res.status(200).json({ message: "No Appoinment Booked" });
         }
         else {
+            const doctor_phone_number = await UserDetails.find({ email: appoinment[0].doctor_email })
             return res.status(200).json({ message: "User appoinment details", appoinment: appoinment, doctor_phone_number: doctor_phone_number[0].phone_number });
         }
     } catch (err) {
@@ -130,6 +138,25 @@ router.post('/cancel', isAuthenticated, isUserValidate, async (req, res) => {
         const { appointment_id } = req.body
         const current_user = req.user["email"];
         const appoinment = await Appoinments.deleteOne({ user: current_user, appointment_id: appointment_id }).select('-_id -__v -created_at -updated_at')
+        if (appoinment.deletedCount === 1) {
+            // Deletion successful, you can handle the response accordingly
+            res.status(200).send({ message: "Appointment deleted successfully" });
+        } else {
+            // Deletion was not successful, handle the error
+            res.status(404).send({ message: "Appointment not found" });
+        }
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({ message: err.message });
+
+    }
+});
+
+router.post('/get-reject', isAuthenticated, isUserValidate, async (req, res) => {
+    try {
+        const { appointment_id } = req.body
+        const current_user = req.user["email"];
+        const appoinment = await Appoinments.find({ user: current_user, appointment_status: { $ne: 'rejected' } }).select('-_id -__v -created_at -updated_at')
         if (appoinment.deletedCount === 1) {
             // Deletion successful, you can handle the response accordingly
             res.status(200).send({ message: "Appointment deleted successfully" });
